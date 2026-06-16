552a36eb5411ea42a609d28b7c2d479e3178d46f
puxesabop77-sys <puxesabop77@gmail.com>
2026-06-16 10:15:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,10 @@ const CONFIG = {
   channelHandle: "@sufidecode",
   particleCount: 120,
   truthMeterDefault: 88,
-  introEnabled: true
+  introEnabled: true,
+
+  youtube: {
+    apiEndpoint: "/api/videos",
+    maxResults: 8
+  }
 };
