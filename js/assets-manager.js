456eb784e03f217ee0c8cae64e53da9b7196d902
puxesabop77-sys <puxesabop77@gmail.com>
2026-06-16 10:21:23 +0530

    Create assets-manager.js

---
+++ js/assets-manager.js
@@ -0,0 +1,56 @@
+const ASSETS = {
+
+images: {
+
+logo:
+"/assets/images/logo.png",
+
+hero:
+"/assets/images/hero-bg.webp",
+
+evidence1:
+"/assets/images/evidence1.webp",
+
+evidence2:
+"/assets/images/evidence2.webp",
+
+evidence3:
+"/assets/images/evidence3.webp",
+
+caseFile:
+"/assets/images/case-file.webp",
+
+classified:
+"/assets/images/classified-stamp.png"
+
+},
+
+sounds: {
+
+click:
+"/assets/sounds/click.mp3",
+
+reveal:
+"/assets/sounds/reveal.mp3",
+
+typing:
+"/assets/sounds/typing.mp3",
+
+notification:
+"/assets/sounds/notification.mp3",
+
+ambient:
+"/assets/sounds/ambient.mp3"
+
+},
+
+video: {
+
+intro:
+"/assets/intro.mp4"
+
+}
+
+};
+
+window.ASSETS = ASSETS;
